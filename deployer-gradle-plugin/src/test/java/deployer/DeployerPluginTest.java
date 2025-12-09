b1603d9c6c6999e3685d24661fda441b7fca8914
Freeman Liu <llw599502537@gmail.com>
2025-12-09 21:49:54 +0800

    use gradle test kit to test plugin

--- deployer-gradle-plugin/src/test/java/deployer/DeployerPluginTest.java
+++ deployer-gradle-plugin/src/test/java/deployer/DeployerPluginTest.java
@@ -2,8 +2,13 @@ package deployer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.gradle.testfixtures.ProjectBuilder;
+import java.io.File;
+import java.nio.file.Files;
+import org.gradle.testkit.runner.BuildResult;
+import org.gradle.testkit.runner.GradleRunner;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 /**
  *
@@ -12,11 +17,47 @@ import org.junit.jupiter.api.Test;
  */
 class DeployerPluginTest {
 
-    @Test
-    void pluginRegistersDeployTask() {
-        var project = ProjectBuilder.builder().build();
-        project.getPlugins().apply("io.github.danielliu1123.deployer");
+    @TempDir
+    File rootDir;
 
-        assertThat(project.getTasks().findByName("deploy")).isNotNull();
+    File settingsFile;
+    File buildFile;
+
+    @BeforeEach
+    void setup() {
+        settingsFile = new File(rootDir, "settings.gradle");
+        buildFile = new File(rootDir, "build.gradle");
+    }
+
+    @Test
+    void testDeployExtension() throws Exception {
+        String settingsGradleContent = """
+                rootProject.name = "deployer-test-project"
+                """;
+        String buildGradleContent = """
+                plugins {
+                    id 'io.github.danielliu1123.deployer'
+                }
+
+                deploy {
+                    dirs = [ file("artifacts") ]
+                    username = "testuser"
+                    password = "testpassword"
+                    publishingType = io.github.danielliu1123.deployer.PublishingType.AUTOMATIC
+                }
+                """;
+
+        Files.writeString(settingsFile.toPath(), settingsGradleContent);
+        Files.writeString(buildFile.toPath(), buildGradleContent);
+
+        BuildResult result = GradleRunner.create()
+                .withProjectDir(rootDir)
+                .withPluginClasspath()
+                .forwardOutput()
+                .withDebug(true) // https://docs.gradle.org/current/userguide/test_kit.html#sub:test-kit-debug
+                .withArguments("deploy", "--dry-run")
+                .build();
+
+        assertThat(result.getOutput()).contains("BUILD SUCCESSFUL");
     }
 }
